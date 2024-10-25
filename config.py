2637a0b572e1ff6d677c7ddd8017242fdf130f63
m.he <49630235+mehdihoore@users.noreply.github.com>
2024-10-26 00:21:33 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,47 @@
+# Config files
+config.py
+.env
+
+# Python
+__pycache__/
+*.py[cod]
+*$py.class
+.Python
+env/
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+*.egg-info/
+.installed.cfg
+*.egg
+
+# Virtual Environment
+venv/
+ENV/
+
+# Temporary files
+temp*/
+*.mp3
+*.wav
+*.txt
+*.pdf
+*.docx
+
+# Telegram session files
+*.session
+*.session-journal
+
+# IDE specific files
+.idea/
+.vscode/
+*.swp
+*.swo
+.DS_Store
